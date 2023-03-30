1cbd2838a1b2e64be9c5c4f361cbda7492c16bd8
Mrowr Purr <mrowr.purr@gmail.com>
2023-03-29 22:09:33 -0700

    Triplets (dynamic, for GTK)

---
+++ triplets/x64-dynamic-by-default.cmake
@@ -0,0 +1,10 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+
+# if(${PORT} MATCHES "luajit|lua-lsqlite3|lua-cjson|pthread")
+# set(VCPKG_CRT_LINKAGE static)
+# set(VCPKG_LIBRARY_LINKAGE static)
+# else()
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
+
+# endif()
