4891427c8fdaa6d0edd97edd71c9c00100365b1c
Mrowr Purr <mrowr.purr@gmail.com>
2023-03-30 22:45:15 -0700

    *Almost* got gtkmm working

--- Examples/Window/WindowExample.cpp
+++ Examples/Window/WindowExample.cpp
@@ -1,49 +1,71 @@
-#include <QApplication>
-#include <QHBoxLayout>
-#include <QLabel>
-#include <QLineEdit>
-#include <QPushButton>
-#include <QTabWidget>
-#include <QVBoxLayout>
-#include <QWidget>
+#include <gtk/gtk.h>
+#include <gtkmm.h>
 
+#include <iostream>
+
+//
+// #include <Windows.h>
+
+// class SimpleWindow : public Gtk::ApplicationWindow {
+// public:
+//     SimpleWindow() {
+//         set_title("Simple Gtkmm App");
+//         set_default_size(200, 100);
+
+//         button.set_label("Click me!");
+//         button.signal_clicked().connect(sigc::mem_fun(*this, &SimpleWindow::on_button_click));
+
+//         set_child(button);
+//         button.show();
+//     }
+
+// protected:
+//     void on_button_click() { std::cout << "Button clicked!" << std::endl; }
+
+//     Gtk::Button button;
+// };
+
+// class SimpleApp : public Gtk::Application {
+// protected:
+//     Glib::RefPtr<Gtk::ApplicationWindow> create_window() {
+//         return Glib::make_refptr_for_instance<SimpleWindow>(new SimpleWindow());
+//     }
+// };
+
+#include <gtkmm/button.h>
+#include <gtkmm/window.h>
+
+// class HelloWorld : public Gtk::Window {
+// public:
+//     HelloWorld();
+//     ~HelloWorld() override;
+
+// protected:
+//     // Signal handlers:
+//     void on_button_clicked();
+
+//     // Member widgets:
+//     Gtk::Button m_button;
+// };
+
+// // int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+// int main(int argc, char* argv[]) {
+//     // auto app = Gtk::Application::create("com.mp.HelloWorld");
+//     // return app->make_window_and_run<HelloWorld>(argc, argv);
+//     auto app = Gtk::Application::create(argc, argv, "");
+// }
+
+class Win : public Gtk::Window {
+public:
+    Win();
+};
+
+Win::Win() {
+    set_title("Hello world");
+    set_default_size(500, 500);
+}
 
 int main(int argc, char* argv[]) {
-    QApplication app(argc, argv);
-    QWidget      window;
-    window.setWindowTitle("Qt Sample");
-    window.resize(300, 200);
-
-    QVBoxLayout* mainLayout = new QVBoxLayout;
-    QTabWidget*  tabWidget  = new QTabWidget;
-
-    // First tab
-    QWidget*     tab1       = new QWidget;
-    QVBoxLayout* tab1Layout = new QVBoxLayout;
-
-    QLabel*      nameLabel  = new QLabel("Name:");
-    QLineEdit*   nameInput  = new QLineEdit;
-    QHBoxLayout* nameLayout = new QHBoxLayout;
-    nameLayout->addWidget(nameLabel);
-    nameLayout->addWidget(nameInput);
-    tab1Layout->addLayout(nameLayout);
-
-    tab1->setLayout(tab1Layout);
-    tabWidget->addTab(tab1, "Tab 1");
-
-    // Second tab
-    QWidget*     tab2       = new QWidget;
-    QVBoxLayout* tab2Layout = new QVBoxLayout;
-
-    QPushButton* button = new QPushButton("Click me");
-    tab2Layout->addWidget(button);
-
-    tab2->setLayout(tab2Layout);
-    tabWidget->addTab(tab2, "Tab 2");
-
-    mainLayout->addWidget(tabWidget);
-    window.setLayout(mainLayout);
-    window.show();
-
-    return app.exec();
+    auto app = Gtk::Application::create("org.gtkmm.examples.base");
+    return app->make_window_and_run<Win>(argc, argv);
 }
