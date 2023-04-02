e51c4a893c360d049dae0b5aa71967a3038e08c0
Mrowr Purr <mrowr.purr@gmail.com>
2023-04-02 08:59:10 -0700

    Update interfaces

--- Library/include/UserInterface/Interfaces.h
+++ Library/include/UserInterface/Interfaces.h
@@ -4,19 +4,39 @@ namespace UserInterface {
 
     struct UIApplication;
 
+    struct UIWidget {
+        virtual ~UIWidget()         = default;
+        virtual const char* GetId() = 0;
+    };
+
+    struct UIButton : public UIWidget {};
+
+    struct UITab {
+        virtual ~UITab()                                                            = default;
+        virtual const char* GetId()                                                 = 0;
+        virtual const char* GetTitle()                                              = 0;
+        virtual void        SetTitle(const char* title)                             = 0;
+        virtual UIWidget*   AddWidget(const char* widgetType, const char* widgetId) = 0;
+    };
+
     struct UIWindow {
-        virtual const char*    GetId()                     = 0;
-        virtual UIApplication* GetApplication()            = 0;
-        virtual bool           Show()                      = 0;
-        virtual bool           SetTitle(const char* title) = 0;
+        virtual ~UIWindow()                                                 = default;
+        virtual const char* GetId()                                         = 0;
+        virtual bool        Show()                                          = 0;
+        virtual bool        SetTitle(const char* title)                     = 0;
+        virtual UITab*      NewTab(const char* tabId, const char* tabTitle) = 0;
+        virtual UITab*      NewTab(const char* tabId) { return NewTab(tabId, tabId); }
+        virtual UIWidget*   AddWidget(const char* widgetType, const char* widgetId) = 0;
     };
 
     struct UIApplication {
+        virtual ~UIApplication()                          = default;
         virtual UIWindow* NewWindow(const char* windowId) = 0;
         virtual void      Run()                           = 0;
     };
 
     struct UIToolkit {
+        virtual ~UIToolkit()                    = default;
         virtual UIApplication* GetApplication() = 0;
     };
 
